3962260ebdcf982bed8a0c28ca47c49c6c55d166
Michael Milton <ttmigueltt@gmail.com>
2021-08-09 14:36:25 +1000

    Add missing files

---
+++ index.js
@@ -0,0 +1,5 @@
+export createRouter from "./createRouter";
+export Link from "./Link";
+export RoutingContext from "./RoutingContext";
+export RouterRenderer from "./RouteRenderer";
+export JSResource from "./JSResource";
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: [
+                            "@babel/plugin-proposal-export-default-from"
+                        ]
+                    }
+                }
+            }
+        ],
+
+    },
+};
